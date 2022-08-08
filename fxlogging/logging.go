335ae83de7bcc9d1f2cd9127df48df46942f6ca6
Aurélien Deroide <aurelien.deroide@exoscale.ch>
2022-08-08 11:47:39 +0200

    Only disable sampling in preprod mode

--- fxlogging/logging.go
+++ fxlogging/logging.go
@@ -55,7 +55,6 @@ func NewLogger(conf LoggingConfig, lc fx.Lifecycle) (*zap.Logger, error) {
 	switch conf.GetLogging().Mode {
 	case "production":
 		config = zap.NewProductionConfig()
-		config.Sampling = nil
 	case "preproduction":
 		config = zap.NewProductionConfig()
 		config.Sampling = nil
