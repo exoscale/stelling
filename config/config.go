171dc2785a8367f7639d82555aa4412fb1b0d10e
Théa Grondier <thea@exoscale.ch>
2023-03-16 15:34:05 +0100

    Config: add a flag to validate parseable duration strings

--- config/config.go
+++ config/config.go
@@ -161,6 +161,12 @@ func registerValidators(validate *validator.Validate) error {
 				return validateExoscaleZoneLong(fl.Field().String()) == nil
 			},
 		},
+		{
+			tag: "duration_flag",
+			validator: func(fl validator.FieldLevel) bool {
+				return validateFlagDuration(fl.Field().String()) == nil
+			},
+		},
 	}
 
 	for _, v := range validators {
