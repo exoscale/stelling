c6618ae02211940bb24623c8d2b81e168f38b03f
Miguel Ping <miguel.ping@exoscale.ch>
2023-06-15 15:17:53 +0100

    use go-systemd but with cached results

--- fxhttp/socket_activation_linux.go
+++ fxhttp/socket_activation_linux.go
@@ -19,83 +19,23 @@ package fxhttp
 import (
 	"fmt"
 	"net"
-	"os"
-	"strconv"
-	"strings"
-	"syscall"
+
+	"github.com/coreos/go-systemd/activation"
 )
 
 // gloval var for the named listeners so we just need to read them once
 var namedListeners map[string][]net.Listener
 
-const (
-	// listenFdsStart corresponds to `SD_LISTEN_FDS_START`.
-	listenFdsStart = 3
-)
-
-// Files returns a slice containing a `os.File` object for each
-// file descriptor passed to this process via systemd fd-passing protocol.
-//
-// The order of the file descriptors is preserved in the returned slice.
-// `unsetEnv` is typically set to `true` in order to avoid clashes in
-// fd usage and to avoid leaking environment flags to child processes.
-func files(unsetEnv bool) []*os.File {
-	if unsetEnv {
-		defer os.Unsetenv("LISTEN_PID")
-		defer os.Unsetenv("LISTEN_FDS")
-		defer os.Unsetenv("LISTEN_FDNAMES")
-	}
-
-	pid, err := strconv.Atoi(os.Getenv("LISTEN_PID"))
-	if err != nil || pid != os.Getpid() {
-		return nil
-	}
-
-	nfds, err := strconv.Atoi(os.Getenv("LISTEN_FDS"))
-	if err != nil || nfds == 0 {
-		return nil
-	}
-
-	names := strings.Split(os.Getenv("LISTEN_FDNAMES"), ":")
-
-	files := make([]*os.File, 0, nfds)
-	for fd := listenFdsStart; fd < listenFdsStart+nfds; fd++ {
-		syscall.CloseOnExec(fd)
-		name := "LISTEN_FD_" + strconv.Itoa(fd)
-		offset := fd - listenFdsStart
-		if offset < len(names) && len(names[offset]) > 0 {
-			name = names[offset]
-		}
-		files = append(files, os.NewFile(uintptr(fd), name))
-	}
-
-	return files
-}
-
-// ListenersWithNames maps a listener name to a set of net.Listener instances.
-func listenersWithNames() map[string][]net.Listener {
-	files := files(true)
-	listeners := map[string][]net.Listener{}
-
-	for _, f := range files {
-		if pc, err := net.FileListener(f); err == nil {
-			current, ok := listeners[f.Name()]
-			if !ok {
-				listeners[f.Name()] = []net.Listener{pc}
-			} else {
-				listeners[f.Name()] = append(current, pc)
-			}
-			f.Close()
-		}
-	}
-	return listeners
-}
-
 // caches the systemd-activated fds and their names
 // Returns the listener associated with the arg
 func NamedSocketListener(name string) (net.Listener, error) {
+	// due to syscall.CloseOnExec(fd), we have to cache the listeners
+	var err error
 	if namedListeners == nil {
-		namedListeners = listenersWithNames()
+		namedListeners, err = activation.ListenersWithNames()
+	}
+	if err != nil {
+		return nil, err
 	}
 
 	namedListeners := namedListeners[name]
