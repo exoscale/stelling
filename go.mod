8ce41f8e8c6cc0ccb0488820131665bc4a1fb6f6
wdullaer <wdullaer@users.noreply.github.com>
2023-06-19 16:41:02 +0200

    feat(sqlite): Add an atomic sqlite migration library (#85)
    The goal is provide an easy and robust way to manage schema's in sqlite.
    
    [sc-70179]
    
    ---------
    
    Co-authored-by: Wouter Dullaert <wouter.dullaert@xoscale.ch>

--- go.mod
+++ go.mod
@@ -26,6 +26,7 @@ require (
 	google.golang.org/grpc v1.52.3
 	google.golang.org/grpc/examples v0.0.0-20230215194445-0f02ca5cc927
 	google.golang.org/protobuf v1.28.1
+	zombiezen.com/go/sqlite v0.13.0
 )
 
 require (
@@ -35,19 +36,23 @@ require (
 	github.com/cenkalti/backoff/v4 v4.2.0 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/dustin/go-humanize v1.0.0 // indirect
 	github.com/fatih/camelcase v1.0.0 // indirect
 	github.com/fatih/structs v1.1.0 // indirect
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/grpc-ecosystem/grpc-gateway/v2 v2.15.0 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/prometheus/client_model v0.3.0 // indirect
 	github.com/prometheus/common v0.39.0 // indirect
 	github.com/prometheus/procfs v0.9.0 // indirect
+	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
 	go.opentelemetry.io/otel/exporters/otlp/internal/retry v1.11.2 // indirect
 	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.11.2 // indirect
 	go.opentelemetry.io/otel/metric v0.34.0 // indirect
@@ -61,4 +66,8 @@ require (
 	golang.org/x/text v0.6.0 // indirect
 	google.golang.org/genproto v0.0.0-20230125152338-dcaf20b6aeaa // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	modernc.org/libc v1.22.3 // indirect
+	modernc.org/mathutil v1.5.0 // indirect
+	modernc.org/memory v1.5.0 // indirect
+	modernc.org/sqlite v1.21.1 // indirect
 )
---
+++ sqlite/migration/migration.go
@@ -0,0 +1,214 @@
+package migration
+
+import (
+	"context"
+	"fmt"
+	"io"
+	"io/fs"
+	"path/filepath"
+	"regexp"
+	"sort"
+	"strconv"
+	"strings"
+
+	"zombiezen.com/go/sqlite"
+	"zombiezen.com/go/sqlite/sqlitex"
+)
+
+const versionSchema string = `CREATE TABLE IF NOT EXISTS schema_migrations (version uint64, dirty bool);
+CREATE UNIQUE INDEX IF NOT EXISTS version_unique ON schema_migrations (version);`
+
+func ensureVersionSchema(conn *sqlite.Conn) (err error) {
+	defer sqlitex.Save(conn)(&err)
+
+	return sqlitex.ExecuteScript(conn, versionSchema, &sqlitex.ExecOptions{})
+}
+
+func dbVersion(conn *sqlite.Conn) (uint64, error) {
+	var version uint64
+	err := sqlitex.ExecuteTransient(
+		conn,
+		"SELECT version FROM schema_migrations LIMIT 1;",
+		&sqlitex.ExecOptions{
+			ResultFunc: func(stmt *sqlite.Stmt) error {
+				version = uint64(stmt.ColumnInt64(0))
+				return nil
+			},
+		},
+	)
+	return version, err
+}
+
+func setDbVersion(conn *sqlite.Conn, version uint64) error {
+	if err := sqlitex.ExecuteTransient(conn, "DELETE FROM schema_migrations;", nil); err != nil {
+		return err
+	}
+	return sqlitex.ExecuteTransient(
+		conn,
+		"INSERT INTO schema_migrations (version, dirty) VALUES (?, ?);",
+		&sqlitex.ExecOptions{Args: []any{version, false}},
+	)
+}
+
+type Migrations struct {
+	upScripts   []string
+	downScripts []string
+}
+
+func NewMigrations(up []string, down []string) (*Migrations, error) {
+	if len(up) != len(down) {
+		return nil, fmt.Errorf("Must have a 'down' migration for each 'up' migration")
+	}
+
+	return &Migrations{
+		upScripts:   up,
+		downScripts: down,
+	}, nil
+}
+
+type migration struct {
+	pos  uint64
+	up   bool
+	name string
+}
+
+type migrationList []*migration
+
+func (m migrationList) Len() int           { return len(m) }
+func (m migrationList) Swap(i, j int)      { m[i], m[j] = m[j], m[i] }
+func (m migrationList) Less(i, j int) bool { return m[i].pos < m[j].pos }
+
+func NewMigrationsFromFS(fsys fs.FS, subpath string) (*Migrations, error) {
+	entries, err := fs.ReadDir(fsys, subpath)
+	if err != nil {
+		return nil, err
+	}
+	upFiles := make([]*migration, 0, len(entries))
+	downFiles := make([]*migration, 0, len(entries))
+	for _, entry := range entries {
+		if entry.IsDir() {
+			continue
+		}
+		if migration, ok := parseMigration(entry.Name()); ok {
+			if migration.up {
+				upFiles = append(upFiles, migration)
+			} else {
+				downFiles = append(downFiles, migration)
+			}
+		}
+	}
+	if len(upFiles) != len(downFiles) {
+		return nil, fmt.Errorf("Target directory must have a 'down' migration for each 'up' migration")
+	}
+	sort.Sort(migrationList(upFiles))
+	sort.Sort(migrationList(downFiles))
+	for i, m := range upFiles {
+		if i != int(m.pos)-1 {
+			return nil, fmt.Errorf("Up migration for migration %d is missing", i+1)
+		}
+	}
+	for i, m := range downFiles {
+		if i != int(m.pos)-1 {
+			return nil, fmt.Errorf("Down migration for migration %d is missing", i+1)
+		}
+	}
+	output := &Migrations{
+		upScripts:   make([]string, len(upFiles)),
+		downScripts: make([]string, len(downFiles)),
+	}
+	for i := range upFiles {
+		if content, err := readString(fsys, subpath, upFiles[i].name); err != nil {
+			return nil, err
+		} else {
+			output.upScripts[i] = content
+		}
+		if content, err := readString(fsys, subpath, downFiles[i].name); err != nil {
+			return nil, err
+		} else {
+			output.downScripts[i] = content
+		}
+	}
+	return output, nil
+}
+
+var filenameRegex = regexp.MustCompile(`([0-9]+)_.*\.(up|down)\.sql`)
+
+func parseMigration(name string) (*migration, bool) {
+	matches := filenameRegex.FindStringSubmatch(name)
+	if len(matches) != 3 {
+		return nil, false
+	}
+	pos, err := strconv.ParseUint(matches[1], 10, 64)
+	if err != nil || pos == 0 {
+		return nil, false
+	}
+	scriptType := matches[2]
+	if scriptType != "up" && scriptType != "down" {
+		return nil, false
+	}
+	return &migration{pos: pos, up: scriptType == "up", name: name}, true
+}
+
+func readString(fsys fs.FS, subpath string, filename string) (string, error) {
+	f, err := fsys.Open(filepath.Join(subpath, filename))
+	if err != nil {
+		return "", err
+	}
+	content := new(strings.Builder)
+	_, err = io.Copy(content, f)
+	f.Close()
+	if err != nil {
+		return "", fmt.Errorf("%s: %w", filename, err)
+	}
+	return content.String(), nil
+}
+
+func (m *Migrations) Up(ctx context.Context, conn *sqlite.Conn) (err error) {
+	targetVersion := uint64(len(m.upScripts))
+	return m.Migrate(ctx, conn, targetVersion)
+}
+
+func (m *Migrations) Down(ctx context.Context, conn *sqlite.Conn) (err error) {
+	return m.Migrate(ctx, conn, 0)
+}
+
+func (m *Migrations) Migrate(ctx context.Context, conn *sqlite.Conn, targetVersion uint64) (err error) {
+	defer sqlitex.Save(conn)(&err)
+
+	if uint64(len(m.upScripts)) < targetVersion {
+		return fmt.Errorf("Target version %d is higher than max migration version %d", targetVersion, len(m.upScripts))
+	}
+
+	if err := ensureVersionSchema(conn); err != nil {
+		return err
+	}
+
+	version, err := dbVersion(conn)
+	if err != nil {
+		return err
+	}
+
+	if version == targetVersion {
+		return nil
+	}
+
+	if uint64(len(m.upScripts)) < version {
+		return fmt.Errorf("Database version %d is higher than max migration version %d", version, len(m.upScripts))
+	}
+
+	if targetVersion < version {
+		for i := int(version - 1); i >= int(targetVersion); i-- {
+			if err := sqlitex.ExecuteScript(conn, m.downScripts[i], nil); err != nil {
+				return err
+			}
+		}
+	} else {
+		for _, migration := range m.upScripts[version:targetVersion] {
+			if err := sqlitex.ExecuteScript(conn, migration, nil); err != nil {
+				return err
+			}
+		}
+	}
+
+	return setDbVersion(conn, targetVersion)
+}
